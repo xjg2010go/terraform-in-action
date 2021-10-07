4fbcc4dda3ddd3734e2b15e651309968a6672269
xjg2010go <xjg2010go@163.com>
2021-10-07 11:51:56 +0800

    add test.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ubuntu" {
     most_recent = true
     filter {
         name  = "name"
-        value = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
     }
     owners = ["099720109477"]
 }
