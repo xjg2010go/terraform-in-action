63cf77b1fd8ff4834ae450feafbc767e8c85fe17
xjg2010go <xjg2010go@163.com>
2021-10-07 12:00:41 +0800

    add test.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "ubuntu" {
     most_recent = true
     filter {
-        name  = "name"
+        name = "name"
         values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
     }
-    owners = ["099720109477"]
+    owners = ["741767866316"]
 }
 
 resource "aws_instance" "helloworld" {
